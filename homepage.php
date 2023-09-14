3f35a245efd8d69b8f00a9c69fc390624e74217f
Jakub Kolář <jk.oolar@gmailcom>
2023-09-14 16:47:47 +0200

    Add video on homepage

--- homepage.php
+++ homepage.php
@@ -10,12 +10,7 @@
 </div>
 
 <div class="container-media">
-  <div class="parallax-wrapper">
-    <?php
-      $hero_image = get_field('hero_image', 'option');
-      echo wp_get_attachment_image($hero_image, 'large', false, ['class' => 'parallax', 'sizes' => '(max-width: 1199px) 100vw, 1200px']);
-    ?>
-  </div>
+  <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/863965124?h=6265f1a510&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Shoptet: We care"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
 </div>
 
 <div class="text-center py-5" id="our-mission">
