930785df21d0efaaddb6702a374e175f21a58e86
Jakub Kolář <jk.oolar@gmailcom>
2023-10-23 10:43:22 +0200

    Different video on HU homepage

--- homepage.php
+++ homepage.php
@@ -10,7 +10,11 @@
 </div>
 
 <div class="container-media">
-  <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/863965124?h=6265f1a510&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Shoptet: We care"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+  <?php if (get_locale() == 'hu_HU'): ?>
+    <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://www.youtube.com/embed/V8CPrfwFpWY?rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" style="position:absolute;top:0;left:0;width:100%;height:100%;" allowfullscreen></iframe></div>
+  <?php else: ?>
+    <div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/863965124?h=6265f1a510&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Shoptet: We care"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+  <?php endif; ?>
 </div>
 
 <div class="text-center py-5" id="our-mission">
